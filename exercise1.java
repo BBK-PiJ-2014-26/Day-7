0277775cb4b6bacb9ba1f2d95bb298a92eff9699
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2014-11-10 18:43:52 +0000

    first commit

---
+++ exercise1.java
@@ -0,0 +1,14 @@
+public class exercise1 {
+
+	public static void main(String[] args) {
+
+			Spy spy1 = new Spy(100);
+			Spy spy2 = new Spy(22);
+			Spy spy3 = new Spy(476);
+			Spy spy4 = new Spy(4125);
+			Spy spy5 = new Spy(1);
+			Spy spy6 = new Spy(007);
+			Spy spy7 = new Spy(9742);
+			Spy spy8 = new Spy(34);
+	}
+}
