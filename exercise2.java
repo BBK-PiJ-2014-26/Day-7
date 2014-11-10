b911076edf439c8923362d8a0948e3b99113fd62
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2014-11-10 19:32:25 +0000

    solved

--- exercise2.java
+++ exercise2.java
@@ -5,13 +5,14 @@ public class exercise2 {
 	public static void main(String[] args) {
 
 		System.out.println("Enter exam marks. Type -1 to terminate.");
+		Exam exam1 = new Exam(-1);
 		int examMark = 0;
 		do  {
 			System.out.println("Input a mark: ");
 			Scanner sc = new Scanner(System.in);
-			int examMark = sc.nextInt();
-			Exam exam = new Exam(examMark);
+			examMark = sc.nextInt();
+			exam1 = new Exam(examMark);
 		} while (examMark != -1);
-		System.out.println("There are " + studentCount + " students: " + distinctionCount + " distinctions, " + passCount + " passes, " + failCount + " fails, (plus " + invalidCount + " invalid entries.");
+		System.out.println("There are " + exam1.studentCount + " students: " + exam1.distinctionCount + " distinctions, " + exam1.passCount + " passes, " + exam1.failCount + " fails, (plus " + exam1.invalidCount + " invalid entries.");
 	}
 }
