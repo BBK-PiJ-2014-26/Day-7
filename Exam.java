3c2c78c37359328c0004d169a74f169b488cd726
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2014-11-10 19:25:38 +0000

    second commit

--- Exam.java
+++ Exam.java
@@ -1,22 +1,22 @@
 public class Exam {
 
-	private int mark;
+	private int examMark;
 	public static int studentCount = 0;
 	public static int distinctionCount = 0;
 	public static int passCount = 0;
 	public static int failCount = 0;
 	public static int invalidCount = 0;
 
-	public Exam(int mark) {
-		if (mark > 100 || mark < -1) {
+	public Exam(int examMark) {
+		if (examMark > 100 || examMark < -1) {
 			invalidCount++;
-		} else if (70 <= mark <= 100) {
+		} else if (70 <= examMark && examMark <= 100) {
 			studentCount++;
 			distinctionCount++;
-		} else if (50 <= mark <= 69) {
+		} else if (50 <= examMark && examMark <= 69) {
 			studentCount++;
 			passCount++;
-		} else if (0 <= mark <= 49) {
+		} else if (0 <= examMark && examMark <= 49) {
 			studentCount++;
 			failCount++;
 		}
